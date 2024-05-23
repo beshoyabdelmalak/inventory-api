e82492157fd99d77813fc9530878eaec7eb712f9
Beshoy Abdelmalak <beshoyibraheem@gmail.com>
2024-05-23 10:18:00 +0200

    Add a product service that will interact with the repository

---
+++ src/main/java/com/example/inventory/services/ProductService.java
@@ -0,0 +1,40 @@
+package com.example.inventory.services;
+
+import com.example.inventory.models.Product;
+import com.example.inventory.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> searchProducts(String query) {
+        return productRepository.findByNameContainingOrBrandContaining(query);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> getLeftoverProducts() {
+        return productRepository.findByQuantityLessThan(5);
+    }
+}
